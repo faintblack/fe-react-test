a66cc2b91ea0d513e83bd770208015c10a606e48
Mahardika Kharisma Ajie <mahardikakharisma@gmail.com>
2023-10-28 14:57:16 +0700

    search and sorting

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,6 +10,11 @@ import {
   CardActionArea,
   CardContent,
   CardMedia,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
   Typography,
 } from "@mui/material";
 import Paginations from "../components/Paginations";
@@ -19,20 +24,65 @@ const Home = () => {
 
   const [currentPage, setCurrentPage] = useState(1);
   const [perPage, setPerPage] = useState(5);
+  const [searchText, setSearchText] = useState("");
+  const [sortBy, setSortBy] = useState("");
 
   const movies = useMemo(() => {
+    let filteredMovies = getMovies.filter(
+      (item) =>
+        item.title.toLowerCase().includes(searchText.toLowerCase()) ||
+        item.desc.toLowerCase().includes(searchText.toLowerCase())
+    );
+
+    // Sorting
+    if (sortBy !== "") {
+      switch (sortBy) {
+        case "rating":
+          filteredMovies = filteredMovies.sort((a, b) => b.rating - a.rating);
+          break;
+        case "title":
+          filteredMovies = filteredMovies.sort((a, b) => {
+            const nameA = a.title.toUpperCase(); // ignore upper and lowercase
+            const nameB = b.title.toUpperCase(); // ignore upper and lowercase
+            if (nameA < nameB) {
+              return -1;
+            }
+            if (nameA > nameB) {
+              return 1;
+            }
+
+            // names must be equal
+            return 0;
+          });
+          break;
+        case "date":
+          filteredMovies = filteredMovies.sort(
+            (a, b) => new Date(b.date) - new Date(a.date)
+          );
+          break;
+      }
+    }
+
     const lastIndex = currentPage * perPage;
     const firstIndex = lastIndex - perPage;
-    const currentItems = getMovies.slice(firstIndex, lastIndex);
+    const currentItems = filteredMovies.slice(firstIndex, lastIndex);
 
     return currentItems;
-  }, [getMovies, currentPage, perPage]);
+  }, [getMovies, currentPage, perPage, searchText, sortBy]);
 
   useEffect(() => {
     console.log("i'm rendered boys");
   }, []);
 
   const _renderItem = (item) => {
+    const date = new Date(item.date);
+    const options = {
+      // weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+
     return (
       <Card sx={{ width: "345px" }}>
         <CardActionArea onClick={() => console.log("masuk pak")}>
@@ -50,6 +100,12 @@ const Home = () => {
             </Typography>
             <Typography variant="body2" color="text.secondary">
               {item.desc}
+            </Typography>
+            <Typography variant="caption" display="block" gutterBottom>
+              {date.toLocaleDateString("en-US", options)}
+            </Typography>
+            <Typography variant="caption" display="block" gutterBottom>
+              {item.rating}
             </Typography>
           </CardContent>
         </CardActionArea>
@@ -69,9 +125,51 @@ const Home = () => {
           },
         ]}
       />
+      {/* Heading */}
       <Typography variant="h3" align="left" gutterBottom>
         Dashboard
       </Typography>
+
+      {/* Sorting */}
+      <Box sx={{ marginBottom: "15px" }}>
+        <Box sx={{ textAlign: "left" }}>
+          <FormControl sx={{ m: 1, minWidth: 80 }} size="small">
+            <InputLabel id="demo-simple-select-autowidth-label">
+              Sort by
+            </InputLabel>
+            <Select
+              labelId="demo-simple-select-autowidth-label"
+              id="demo-simple-select-autowidth"
+              value={sortBy}
+              onChange={(e) => setSortBy(e.target.value)}
+              autoWidth
+              label="Sort by"
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value="title">Title</MenuItem>
+              <MenuItem value="date">Date</MenuItem>
+              <MenuItem value="rating">Rating</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
+
+        <TextField
+          id="search-bar"
+          label="Search"
+          variant="outlined"
+          size="small"
+          onChange={(e) => {
+            setSearchText(e.target.value);
+            setCurrentPage(1);
+          }}
+          sx={{
+            width: "100%",
+            margin: "0 8px",
+          }}
+        />
+      </Box>
 
       {/* List Item */}
       <Box
@@ -87,7 +185,7 @@ const Home = () => {
 
       {/* Pagination */}
       <Paginations
-        totalItem={getMovies.length}
+        totalItem={searchText === "" ? getMovies.length : movies.length}
         perPage={perPage}
         currentPage={currentPage}
         setPage={(e) => setCurrentPage(e)}
